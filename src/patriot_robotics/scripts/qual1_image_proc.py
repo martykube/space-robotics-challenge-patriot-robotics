c16c7a74113d48fca481b494c92cf5c615bf365e
Marty Kube <marty@beavercreekconsulting.com>
2016-12-18 15:13:12 -0500

    Added LED detection

--- src/patriot_robotics/scripts/qual1_image_proc.py
+++ src/patriot_robotics/scripts/qual1_image_proc.py
@@ -11,30 +11,41 @@ from std_msgs.msg import Empty
 
 class Qual1ImageProc:
     '''
-    Subscribe to an image queue, process images, and publish ROI
+    Process images and do two things:
+    - Identify when the big screen  turns on or off
+    - Identify when LEDs turn on or off and publish image coordinates
     Subscribes:
-     - image
+    - image
     Publishes:
-     - led_location msg/??
-     - led_annotated sensor_msgs/Image
+    - on When white screen is on
+    - off When white screen is off
+    - acquired When LED is detected
+    - lost when no LED is detected
+    
+    - on_off_annotated Debuging image for white screen detector
+    - led_location LED image coordinates
+    - led_annotated Debugging image for LED
     '''
 
 
     def __init__(self):        
         self.bridge = CvBridge()
+        self.first_image = None
+
+        # images to be processed
+        rospy.Subscriber("image", Image, self.process_image)
 
         # Task start stop - big white screen
         self.on_publisher = rospy.Publisher("on", Empty, queue_size = 10)
         self.off_publisher = rospy.Publisher("off", Empty, queue_size = 10)
         self.on_off_annotated_publisher = rospy.Publisher(
-            "qual1_image_proc/on_off_annotated", Image, queue_size = 2)
+            "on_off_annotated", Image, queue_size = 2)
 
+        # LED detector
+        self.acquired_publisher = rospy.Publisher("acquired", Empty, queue_size = 10)
+        self.lost_publisher = rospy.Publisher("lost", Empty, queue_size = 10)
         self.image_publisher =  rospy.Publisher("led_annotated", Image, queue_size = 2)
-        self.point_publisher =  rospy.Publisher("led_point", Point, queue_size = 2)
-
-        self.first_image = None
-
-        rospy.Subscriber("image", Image, self.process_image)
+        self.point_publisher =  rospy.Publisher("led_point", Point, queue_size = 10)
 
 
     def process_image(self, image):
@@ -43,7 +54,7 @@ class Qual1ImageProc:
         Check for LEDs
         '''
         self.check_start_stop(image)
-        # self.find_led(image)
+        self.find_led(image)
 
 
     def gray_pre_process(self, image):
@@ -105,12 +116,14 @@ class Qual1ImageProc:
         red_mask = cv2.inRange(image_hsv, (0, 127, 127), (1, 255, 255))
 
         image_with_contours = None
+        acquired = False
         for mask in (blue_mask, green_mask, red_mask):
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, 
                                                    cv2.CHAIN_APPROX_SIMPLE)
 
             # if we find a contour, use it...
             if len(contours) > 0:
+                acquired = True
 
                 # publish annotated image
                 cv2.drawContours(cv2_img_bgr, contours, 0, (20, 255, 57), 3)
@@ -128,6 +141,11 @@ class Qual1ImageProc:
                 self.point_publisher.publish(point)
 
                 break
+
+        if acquired:
+            self.acquired_publisher.publish(Empty())
+        else:
+            self.lost_publisher.publish(Empty())
 
 
 if __name__ == '__main__':

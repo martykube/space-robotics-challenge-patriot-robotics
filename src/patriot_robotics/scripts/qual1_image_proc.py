6af78d73e0f9dd3710249bd0ed0ddc61670aa1cd
Marty Kube <marty@beavercreekconsulting.com>
2016-12-19 20:29:15 -0500

    Ignore start stop and simplify state machine

--- src/patriot_robotics/scripts/qual1_image_proc.py
+++ src/patriot_robotics/scripts/qual1_image_proc.py
@@ -13,17 +13,14 @@ from std_msgs.msg import Empty
 
 class Qual1ImageProc:
     '''
-    Process images and do two things:
-    - Identify when the big screen  turns on or off
-    - Identify when LEDs turn on or off and publish image coordinates
+    Identify when LEDs turn on or off and publish image coordinates
+
     Subscribes:
     - image
+
     Publishes:
-    - on When white screen is on
-    - off When white screen is off
     - acquired When LED is detected
-    - lost when no LED is detected
-    
+    - lost when no LED is detected    
     - on_off_annotated Debuging image for white screen detector
     - led_location LED image coordinates
     - led_annotated Debugging image for LED
@@ -36,74 +33,13 @@ class Qual1ImageProc:
         # images to be processed
         rospy.Subscriber("image", Image, self.process_image)
 
-        # Task start stop - big white screen
-        self.on_publisher = rospy.Publisher("on", Empty, queue_size = 20)
-        self.off_publisher = rospy.Publisher("off", Empty, queue_size = 20)
-        self.on_off_annotated_publisher = rospy.Publisher(
-            "on_off_annotated", Image, queue_size = 20)
-
         # LED detector
         self.acquired_publisher = rospy.Publisher("acquired", Empty, queue_size = 20)
         self.lost_publisher = rospy.Publisher("lost", Empty, queue_size = 20)
         self.image_publisher =  rospy.Publisher("led_annotated", Image, queue_size = 20)
         self.point_publisher =  rospy.Publisher("led_point", PointStamped, queue_size = 20)
 
-
     def process_image(self, image):
-        '''
-        Check for task start stop indicator
-        Check for LEDs
-        '''
-        self.check_start_stop(image)
-        self.find_led(image)
-
-
-    def gray_pre_process(self, image):
-        gray = self.bridge.imgmsg_to_cv2(image, desired_encoding="mono8")
-        blur = cv2.GaussianBlur(gray, (15, 15), 0)
-        return blur
-
-
-    def check_start_stop(self, image):
-        ''' 
-        Use gray scale images.  Subtract first image and threshold.  
-        Look for a big white blob covering the middle of the image.  
-        '''
-        if self.first_image is None:
-            self.first_image = self.gray_pre_process(image)
-            return
-            
-        task_on = False
-        img_bgr = self.bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
-
-        image = self.gray_pre_process(image)
-        delta = cv2.absdiff(self.first_image, image)
-        ret, thresh = cv2.threshold(delta, 127, 255, cv2.THRESH_BINARY) 
-        contours, hierarchy = cv2.findContours(
-            np.copy(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        image_with_contours = np.copy(img_bgr)
-
-        # If we find a contour which contains the image center, use it
-        center = (thresh.shape[1] / 2, thresh.shape[0] / 2)
-        for contour in contours:
-            location = cv2.pointPolygonTest(contour, center, False)
-            if location > -1:
-                task_on = True
-                cv2.drawContours(image_with_contours, [contour], 0, (20, 255, 57), 3)
-                break
-
-        # report on or off status
-        if task_on:
-            self.on_publisher.publish(Empty())
-        else:
-            self.off_publisher.publish(Empty())
-        
-        # publish annotated image for debugging
-        self.on_off_annotated_publisher.publish(
-            self.bridge.cv2_to_imgmsg(image_with_contours, 'bgr8'))
-
-
-    def find_led(self, image):
         '''
         Convert inbound image to OpenCV.
         Apply LED detector
@@ -118,6 +54,7 @@ class Qual1ImageProc:
 
         image_with_contours = None
         acquired = False
+
         for mask in (blue_mask, green_mask, red_mask):
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, 
                                                    cv2.CHAIN_APPROX_SIMPLE)
--- src/patriot_robotics/scripts/qual1_state.py
+++ src/patriot_robotics/scripts/qual1_state.py
@@ -9,16 +9,12 @@ from srcsim.msg import Console
 class State:
     '''
     Sate machine for Qual 1.  States:
-    - off      Initial state and task complete state
-    - seeking  State when task starts (big white screen turns on) or tracking is lost
-    - tracking State when LED is acquired
+    - seeking  Looking for LED  Message to acquired transitions state to tracking
+    - tracking Found a LED  Message to lost transitions state to seeking
     '''
     
     def __init__(self):
-        self.state = 'off'
         self.published_led = False
-        rospy.Subscriber('on', Empty, self.on)
-        rospy.Subscriber('off', Empty, self.off)
         rospy.Subscriber('acquired', Empty, self.acquired)
         rospy.Subscriber('lost', Empty, self.lost)
         rospy.Subscriber('led_3D_location', PointStampedColorRGBA, 
@@ -26,26 +22,18 @@ class State:
         
         self.console_publisher = rospy.Publisher("/srcsim/qual1/light", 
                                             Console, queue_size = 20)
-
-    def on(self, msg):
-        if self.state == 'off':
-            self.state= 'seeking'
-            self.published_led = False
-            self.log_state()
-        
-    def off(self, msg):
-        if self.state == 'seeking':
-            self.state= 'off'
-            self.log_state()
+        self.seeking = 'seeking'
+        self.tracking = 'tracking'
+        self.state = self.seeking
 
     def acquired(self, msg):
-        if self.state == 'seeking':
-            self.state= 'tracking'
+        if self.state == self.seeking:
+            self.state= self.tracking
             self.log_state()
 
     def lost(self, msg):
-        if self.state == 'tracking':
-            self.state= 'seeking'
+        if self.state == self.tracking:
+            self.state= self.seeking
             self.published_led = False
             self.log_state()
 
@@ -68,12 +56,10 @@ class State:
             self.console_publisher.publish(console_msg)
             self.published_led = True
             rospy.loginfo("Published LED")
-        
 
     def log_state(self):
         rospy.loginfo('State is %s' % self.state)
         
-
 if __name__ == '__main__':
     rospy.init_node('qual1_state')
     state = State()

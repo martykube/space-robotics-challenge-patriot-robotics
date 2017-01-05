ff83f92b8a1cc9b3e6dd9eb2f5135ae2139a8489
Erica S. Kane <ens@ix.netcom.com>
2017-01-04 20:04:26 -0500

    Added debug to armDemo.py

--- src/patriot_robotics/scripts/armDemo.py
+++ src/patriot_robotics/scripts/armDemo.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         armTrajectoryPublisher = rospy.Publisher("/ihmc_ros/{0}/control/arm_trajectory".format(ROBOT_NAME), ArmTrajectoryRosMessage, queue_size=1)
 
         rate = rospy.Rate(10) # 10hz
-        time.sleep(1)
+        time.sleep(1) 
 
         # make sure the simulation is running otherwise wait
         if armTrajectoryPublisher.get_num_connections() == 0:

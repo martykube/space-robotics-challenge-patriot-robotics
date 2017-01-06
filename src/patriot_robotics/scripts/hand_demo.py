658f30c0a66cab6a773216af39bfff24cda24a04
Erica S. Kane <ens@ix.netcom.com>
2017-01-05 20:18:49 -0500

    Fix wrong version that was uploaded somehow.

--- src/patriot_robotics/scripts/hand_demo.py
+++ src/patriot_robotics/scripts/hand_demo.py
@@ -6,20 +6,27 @@ import rospy
 import tf
 import tf2_ros
 import numpy
+import std_msgs
+import geometry_msgs
 
 from ihmc_msgs.msg import HandTrajectoryRosMessage
 from ihmc_msgs.msg import HandDesiredConfigurationRosMessage
 from ihmc_msgs.msg import SE3TrajectoryPointRosMessage
+from geometry_msgs.msg import PointStamped, PoseStamped
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import TransformStamped
 
 ROBOT_NAME = None
-
-LEFT = 0
-RIGHT = 1
 
 RIGHT_HAND_FRAME_NAME = "rightPalm"
 LEFT_HAND_FRAME_NAME = "leftPalm"
 
+'''
+A simple demo which moves the right hand out then back in, from it's current position.
+'''
 def sendRightHandTrajectory():
+    handStart = getCurrentPosition(HandTrajectoryRosMessage.RIGHT)
+
     # This message commands the controller to move in taskspace a hand to the desired pose (position &
     # orientation) while going through the specified trajectory points. A third order polynomial function
     # is used to interpolate positions and a hermite based curve (third order) is used to interpolate the
@@ -32,46 +39,58 @@ def sendRightHandTrajectory():
     msg.execution_mode = HandTrajectoryRosMessage.OVERRIDE
     msg.unique_id = rospy.Time.now().nsecs
 
-    trajectory = createHandOffset(LEFT, [0.2, 0.0, 0.2])
+    trajectory = createHandOffset(HandTrajectoryRosMessage.RIGHT, [0.0, -0.2, -0.2])
     trajectory.time = 1
     msg.taskspace_trajectory_points.append(trajectory)
 
-    rospy.loginfo('publishing right hand trajectory')
+    rospy.loginfo('publishing first right hand trajectory')
     handTrajectoryPublisher.publish(msg)
+
+    time.sleep(5)
+    
+    # trajectory = createHandOffset(HandTrajectoryRosMessage.RIGHT, [0.0, 0.2, 0.2])
+    trajectory.position = handStart.position
+    trajectory.orientation = handStart.orientation
+    trajectory.time = 1
+    msg.previous_message_id = msg.unique_id
+    msg.unique_id = rospy.Time.now().nsecs
+    msg.execution_mode = HandTrajectoryRosMessage.QUEUE
+    msg.taskspace_trajectory_points[0] = trajectory
+ 
+    rospy.loginfo('publishing second right hand trajectory')
+    handTrajectoryPublisher.publish(msg)
+
 
 # Get current position and orientation of the hand in world frame as a SE3TrajectoryPointRosMessage
 def getCurrentPosition(stepSide):
     handstep = SE3TrajectoryPointRosMessage()
 
-    if stepSide == LEFT:
+    if stepSide == HandTrajectoryRosMessage.LEFT:
         hand_frame = LEFT_HAND_FRAME_NAME
     else:
-        frame_frame = RIGHT_HAND_FRAME_NAME
+        hand_frame = RIGHT_HAND_FRAME_NAME
 
     handWorld = tfBuffer.lookup_transform('world', hand_frame, rospy.Time())
     handstep.orientation = handWorld.transform.rotation
     handstep.position = handWorld.transform.translation
 
+    rospy.loginfo('current world position: {0}'.format(handstep.position))
+
     return handstep
 
 def createHandOffset(stepSide, offset):
     handstep = getCurrentPosition(stepSide)
-    handstep.linear_velocity.x = 0;
-    handstep.linear_velocity.y = 0;
-    handstep.linear_velocity.z = 0;
-    handstep.angular_velocity.x = 0;
-    handstep.angular_velocity.y = 0;
-    handstep.angular_velocity.z = 0;
 
-    quat = handstep.orientation
-    rot = tf.transformations.quaternion_matrix([quat.x, quat.y, quat.z, quat.w])
-    transformedOffset = numpy.dot(rot[0:3, 0:3], offset)
+    # do the move
+    handstep.position.x += offset[0];
+    handstep.position.y += offset[1];
+    handstep.position.z += offset[2];
 
     return handstep
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('ihmc_hand_demo')
+        rospy.init_node('patriot_hand_demo')
 
         ROBOT_NAME = rospy.get_param('/ihmc_ros/robot_name')
 	rospy.loginfo("robot name: {0}".format(ROBOT_NAME))
